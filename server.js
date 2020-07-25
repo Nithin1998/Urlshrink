6ca64ecdab0fc23cf93133bd87a066bc66939d53
NITHIN GEORGE PHILIPS <nphilips914@gmail.com>
2020-07-25 12:39:38 +0530

    file rendering

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app = express();
 app.use(express.static('build'));
 
 app.get('/',(req,res)=>{
-    app.send('index')
+    app.render('index.html')
 })
 
 app.listen(process.env.PORT || 3000,()=>console.log("server running"))
