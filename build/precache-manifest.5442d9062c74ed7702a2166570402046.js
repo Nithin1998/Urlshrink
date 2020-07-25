cc523ef0190e2b95456e866c31628ab1be29e383
NITHIN GEORGE PHILIPS <nphilips914@gmail.com>
2020-07-25 11:24:57 +0530

    optimized

---
+++ build/precache-manifest.5442d9062c74ed7702a2166570402046.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17eb5279bb10c2efbc166ce46baa81ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce26b8249e5401caa0d2",
+    "url": "/static/css/main.c192947a.chunk.css"
+  },
+  {
+    "revision": "795ef39d1a627f7a8b44",
+    "url": "/static/js/2.0891f346.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0891f346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce26b8249e5401caa0d2",
+    "url": "/static/js/main.3e8ff28e.chunk.js"
+  },
+  {
+    "revision": "ad3d3f1d34a73b724b01",
+    "url": "/static/js/runtime-main.b1bfe79c.js"
+  }
+]);
