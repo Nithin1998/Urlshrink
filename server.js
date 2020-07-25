36ecc00b8e68b288328959825cc404d5592d8214
NITHIN GEORGE PHILIPS <nphilips914@gmail.com>
2020-07-25 11:51:40 +0530

    new files

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const express = require("express");
-const app = express();
-
-app.use(express.static('build'))
+const express = require('express');
+const path = require('path');
+app = express();
+app.use( express.static(path.join(__dirname, 'public')));
 
 app.get('/',(req,res)=>{
-    res.send('index.html')
+    app.send('index.html')
 })
 
-
-app.listen(3000,console.log("app running"))
+app.run(process.env.PORT || 3000);
